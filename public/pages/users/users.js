8830b93b93a8d937b49fca9fc29d795cc9d482a3
Cindy Salinas <cindysalinas15@gmail.com>
2017-06-13 14:34:39 -0400

    get users and save user

--- public/pages/users/users.js
+++ public/pages/users/users.js
@@ -1,8 +1,9 @@
-
 angular.module('app.users', [])
-  .controller("UsersCtrl", ['$scope', function ($scope) {
+  .controller('UsersCtrl', ['$scope', 'ApiRequests', function($scope, ApiRequests) {
 
-    //datepicker options
+    $scope.user = {};
+    $scope.userList = [];
+
     $scope.dateOptions = {
       formatYear: 'yyyy',
       maxDate: new Date(),
@@ -17,4 +18,29 @@ angular.module('app.users', [])
       opened: false
     };
 
-}]);
+    $scope.getUsers = function() {
+      ApiRequests.getUsers()
+      .then(function(res) {
+        console.log(res);
+        $scope.userList = res.data;
+      })
+      .catch(function(err){
+        console.log('err', err);
+      });
+    }
+    $scope.dateChange = function(date) {
+      $scope.user.birthDate = date;
+    }
+
+    $scope.createUser = function(data) {
+      // console.log(data);
+      ApiRequests.saveUser(data)
+        .then(function(res) {
+          console.log('res', res);
+          $scope.user = {};
+        })
+        .catch(function(err) {
+          console.log('err', err);
+        });
+    }
+  }]);
