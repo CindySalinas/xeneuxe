8c7aa7eb9e9d60f39ea01eb90a3b5140d6467d11
Cindy Salinas <cindysalinas15@gmail.com>
2017-06-13 10:40:01 -0400

    added dependencies

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
 
 var express = require('express'),
     app = express(),
-    expressValidator = require('express-validator')
     bodyParser = require('body-parser'),
     mongoose = require('mongoose'),
     config = require('./config/config');
 
+
+//bodyParser
+app.use(bodyParser.json());
 //view
 app.use(express.static(__dirname + '/build'));
+
+//database
+/*mongoose.Promise = global.Promise;
+mongoose.connect(config.db.url, function(err, res) {
+  if(err){
+    console.log('Sorry, there is no mongo db server running.');
+    throw err;
+  }
+  console.log('Successfully connected to mongodb');
+});*/
 
 var server = app.listen(config.port || 8080, function() {
   var host = server.address().address;
